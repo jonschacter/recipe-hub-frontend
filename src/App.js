27031ae578fa62d49db9c2b52e72f2b6e53aed73
github username <schacter.jon@gmail.com>
2020-10-14 14:30:25 -0700

    initial react-redux setup and welcome page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-class App extends React.Component {
+import Welcome from './components/Welcome.js'
+
+class App extends Component {
     render(){
         return (
             <div className="App">
-                App
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Welcome} />
+                    </Switch>
+                </Router>
             </div>
         );
     }
