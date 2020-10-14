79c544faa844fc677adbfbafb3db0b6f5fe94cbc
github username <schacter.jon@gmail.com>
2020-10-14 14:43:56 -0700

    add routes for /login and /signup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Welcome from './components/Welcome.js'
+import UserForm from './components/user/UserForm.js'
 
 class App extends Component {
     render(){
@@ -10,6 +11,8 @@ class App extends Component {
                 <Router>
                     <Switch>
                         <Route exact path="/" component={Welcome} />
+                        <Route exact path="/login" render={(routerProps) => <UserForm {...routerProps} type="Log In" />} />
+                        <Route exact path="/signup" render={(routerProps) => <UserForm {...routerProps} type="Sign Up" />} />
                     </Switch>
                 </Router>
             </div>
