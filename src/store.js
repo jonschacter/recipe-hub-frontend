31b75dda311a152bd8e09423a74116ea3249eee7
github username <schacter.jon@gmail.com>
2020-10-24 13:31:07 -0700

    add recipe reducer to store combineReducers

--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 
 import currentUser from './reducers/currentUser.js'
+import recipes from './reducers/recipes.js'
 
 const reducer = combineReducers({
-    currentUser
+    currentUser,
+    recipes
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
