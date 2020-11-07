f96f1983ee804b695bef8ec5f2cfbd54ba71fe3e
github username <schacter.jon@gmail.com>
2020-11-06 17:07:52 -0800

    create SetRecipe action to fetch from API

--- src/actions/recipes.js
+++ src/actions/recipes.js
@@ -5,6 +5,13 @@ export const setRecipes = recipes => {
     }
 }
 
+export const setRecipe = recipe => {
+    return {
+        type: "SET_RECIPE",
+        recipe
+    }
+}
+
 export const getRecipes = () => {
     return dispatch => {
         return fetch('http://localhost:3001/api/v1/recipes', {
@@ -17,6 +24,22 @@ export const getRecipes = () => {
             .then(resp => resp.json())
             .then(data => {
                 dispatch(setRecipes(data))
+            })
+            .catch(error => alert(error))
+    }
+}
+
+export const getRecipe = (id) => {
+    return dispatch => {
+        return fetch(`http://localhost:3001/api/v1/recipes/${id}`, {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }})
+            .then(resp => resp.json())
+            .then(data => {
+                dispatch(setRecipe(data))
             })
             .catch(error => alert(error))
     }
