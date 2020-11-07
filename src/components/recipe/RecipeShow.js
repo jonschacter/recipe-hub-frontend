5d8dfc23b5a153a0ad35838b9612969012c82ec0
github username <schacter.jon@gmail.com>
2020-11-06 17:24:58 -0800

    component fetches info on mount with getRecipe action, athen renders basic info

--- src/components/recipe/RecipeShow.js
+++ src/components/recipe/RecipeShow.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getRecipe } from '../../actions/recipes.js'
 
-const RecipeShow = () => {
-   return(
-       <div>Recipe Show page info</div>
-   ) 
+class RecipeShow extends Component {
+    componentDidMount(){
+        const { getRecipe, match } = this.props
+        getRecipe(match.params.id)
+    }
+    
+    renderReview = () => {
+        const { recipe } = this.props
+        return(
+            <div>
+                <h2>{ recipe.name }</h2>
+                <p>Rating: { recipe.rating ? recipe.rating : <>N/A</> }</p>
+                <p>Category: { recipe.category.name }</p>
+                <p>Ingredients: </p>
+                <ul>
+                    { recipe.ingredients.map(ingredient => {
+                        return <li>{ ingredient.name }</li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+
+    render(){
+        const { recipe, match } = this.props
+        return(
+            <div>
+                { recipe.id === parseInt(match.params.id) ? this.renderReview() : <h3>LOADING...</h3> }
+            </div>
+        )
+    }
 }
 
-export default RecipeShow
+const mapStateToProps = ({ recipe }) => {
+    return {
+        recipe
+    }
+}
+
+export default connect(mapStateToProps, { getRecipe })(RecipeShow)
