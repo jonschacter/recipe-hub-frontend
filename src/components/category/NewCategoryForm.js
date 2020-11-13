66b6db3f47974af51fd1dbc9a3a606743605bceb
github username <schacter.jon@gmail.com>
2020-11-13 14:58:05 -0800

    connect to createCategory action, and handleSubmit

--- src/components/category/NewCategoryForm.js
+++ src/components/category/NewCategoryForm.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createCategory } from '../../actions/categories.js'
 
 class NewCategoryForm extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            name: ""
+        }
+    }
+    
+    handleChange = ({ target }) => {
+        this.setState({
+            name: target.value
+        })
+    }
+
+    handleSubmit = event => {
+        // not sure i want to prevent default
+        event.preventDefault()
+        
+        this.props.createCategory({ name: this.state.name })
+    }
+
     render(){
         return(
-            <div>New Category Form</div>
+            <form onSubmit={this.handleSubmit}>
+                <label>Category Name:</label>
+                <input type="text" name="name" onChange={this.handleChange} value={this.state.name} />
+                <br />
+                <input type="submit" value="Create Category" />
+            </form>
         )
     }
 }
 
-export default NewCategoryForm
+export default connect(null, { createCategory })(NewCategoryForm)
